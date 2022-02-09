5242342b53c2495bcbaa0c973578dfc319a825ad
Favour <toluwaniokunowo@gmail.com>
2022-02-09 05:06:10 +0100

    create nav component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-link :to="to" class="bg-white rounded-lg p-3 text-center uppercase font-bold cursor-pointer">
+        <slot />
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "NavLink",
+    props: ['to']
+}
+</script>
