70a8170274479a21ddaf7acc192038601de7980d
Favour <toluwaniokunowo@gmail.com>
2022-02-20 23:33:31 +0100

    define custom colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // qrcode color
+                'qr-primary': '#D5E1EF',
+                // stats color
+                'stats-dark-blue': 'hsl(233, 47%, 7%)',
+                'stats-desaturated-blue': 'hsl(244, 38%, 16%)',
+                'stats-soft-violet': 'hsl(277, 64%, 61%)',
+                'stats-white': 'hsl(0, 0%, 100%)',
+                'stats-trans-white': 'hsla(0, 0%, 100%, 0.75)',
+                'stats-trans-white-headings': 'hsla(0, 0%, 100%, 0.6)',
+            }
+        },
     },
     plugins: [],
 }
