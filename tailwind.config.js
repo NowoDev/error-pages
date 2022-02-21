3cd569d268ad5c8906ad91f0317522e75473aa2c
Favour <toluwaniokunowo@gmail.com>
2022-02-21 14:37:02 +0100

    configure screen & new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
         "./index.html",
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
+        screens: {
+            'xs': '375px',
+            ...defaultTheme.screens,
+        },
         extend: {
             colors: {
                 // qrcode color
                 'qr-primary': '#D5E1EF',
+
                 // stats color
                 'stats-dark-blue': 'hsl(233, 47%, 7%)',
                 'stats-desaturated-blue': 'hsl(244, 38%, 16%)',
@@ -15,6 +22,12 @@ module.exports = {
                 'stats-white': 'hsl(0, 0%, 100%)',
                 'stats-trans-white': 'hsla(0, 0%, 100%, 0.75)',
                 'stats-trans-white-headings': 'hsla(0, 0%, 100%, 0.6)',
+
+                // article color
+                'article-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+                'article-dark-blue': 'hsl(214, 17%, 51%)',
+                'article-grayish-blue': 'hsl(212, 23%, 69%)',
+                'article-light-grayish-blue': 'hsl(210, 46%, 95%)',
             }
         },
     },
