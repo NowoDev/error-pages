4cf72e0cacd6cf09ce274064e34f7e8b3e8f9b78
Favour <toluwaniokunowo@gmail.com>
2022-03-10 16:48:59 +0100

    show active states
    Show shadow on button hover,underline navbar items on hover, add thumbnail images

--- src/Pages/Challenges/ECommerce/Index.vue
+++ src/Pages/Challenges/ECommerce/Index.vue
@@ -1,70 +1,79 @@
 <template>
-    <div>
-        <nav class="p-5 bg-white">
-            <div class="flex justify-between mx-auto max-w-7xl">
-                <div class="flex items-center space-x-4 w-full md:space-x-0">
-                    <button class="md:hidden" @click="toggleNav">
-                        <svg class="w-8 h-8 text-e-dark-grayish-blue" fill="currentColor"
-                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path clip-rule="evenodd"
-                                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                  fill-rule="evenodd" />
-                        </svg>
-                    </button>
-
-                    <img alt="sneakers" src="../../../assets/images/ecommerce/logo.svg">
-
-                    <div class="hidden items-center pl-9 space-x-4 md:flex">
-                        <h3 class="text-lg font-bold text-e-very-dark-blue">Collections</h3>
-                        <h3 class="text-lg font-bold text-e-very-dark-blue">Men</h3>
-                        <h3 class="text-lg font-bold text-e-very-dark-blue">Women</h3>
-                        <h3 class="text-lg font-bold text-e-very-dark-blue">About</h3>
-                        <h3 class="text-lg font-bold text-e-very-dark-blue">Contact</h3>
-                    </div>
-                </div>
-
-                <div class="flex items-center space-x-6">
-                    <button @click="toggleCart">
-                        <svg class="w-6 h-6 text-e-dark-grayish-blue" fill="none" stroke="currentColor"
-                             stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round" />
-                        </svg>
-                    </button>
-
-                    <img alt="Avatar" class="w-8" src="../../../assets/images/ecommerce/image-avatar.png">
-                </div>
-            </div>
-
-            <!-- mobile nav-->
-            <div :class="showNav === true ? 'block' : 'hidden'"
-                 class="absolute inset-0 z-50 p-6 w-2/3 h-screen bg-white">
-                <button @click="toggleNav">
-                    <svg class="w-6 h-6 text-e-dark-grayish-blue" fill="currentColor" viewBox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg">
+    <nav class="fixed z-10 p-5 w-full bg-white border-b md:px-10 md:py-0">
+        <div class="flex justify-between mx-auto max-w-7xl">
+            <div class="flex items-center space-x-4 w-full md:space-x-0">
+                <button class="md:hidden" @click="toggleNav">
+                    <svg class="w-8 h-8 text-e-dark-grayish-blue" fill="currentColor"
+                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path clip-rule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                               fill-rule="evenodd" />
                     </svg>
                 </button>
-                <div class="py-12 space-y-4">
-                    <h3 class="text-lg font-bold text-e-very-dark-blue">Collections</h3>
-                    <h3 class="text-lg font-bold text-e-very-dark-blue">Men</h3>
-                    <h3 class="text-lg font-bold text-e-very-dark-blue">Women</h3>
-                    <h3 class="text-lg font-bold text-e-very-dark-blue">About</h3>
-                    <h3 class="text-lg font-bold text-e-very-dark-blue">Contact</h3>
+
+                <img alt="sneakers" src="../../../assets/images/ecommerce/logo.svg">
+
+                <div class="hidden items-center pl-9 space-x-10 md:flex">
+                    <h3 class="py-10 text-lg cursor-pointer hover:border-b-e-orange hover:border-b-4 text-e-very-dark-blue">
+                        Collections</h3>
+                    <h3 class="py-10 text-lg cursor-pointer hover:border-b-e-orange hover:border-b-4 text-e-very-dark-blue">
+                        Men</h3>
+                    <h3 class="py-10 text-lg cursor-pointer hover:border-b-e-orange hover:border-b-4 text-e-very-dark-blue">
+                        Women</h3>
+                    <h3 class="py-10 text-lg cursor-pointer hover:border-b-e-orange hover:border-b-4 text-e-very-dark-blue">
+                        About</h3>
+                    <h3 class="py-10 text-lg cursor-pointer hover:border-b-e-orange hover:border-b-4 text-e-very-dark-blue">
+                        Contact</h3>
                 </div>
             </div>
-        </nav>
 
-        <main :class="showNav === true ? 'blur' : 'blur-0'" class="mx-auto max-w-7xl bg-white">
+            <div class="flex items-center mr-5 space-x-6">
+                <button @click="toggleCart">
+                    <svg class="w-6 h-6 text-e-dark-grayish-blue hover:text-black" fill="none" stroke="currentColor"
+                         stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                              stroke-linecap="round"
+                              stroke-linejoin="round" />
+                    </svg>
+                </button>
+
+                <img alt="Avatar"
+                     class="w-8 rounded-full transition duration-300 ease-in-out cursor-pointer hover:ring-2 md:w-12 hover:scale-110 hover:ring-e-orange"
+                     src="../../../assets/images/ecommerce/image-avatar.png">
+            </div>
+        </div>
+
+        <!-- mobile nav-->
+        <div :class="showNav === true ? 'block' : 'hidden'"
+             class="absolute inset-0 z-50 p-6 w-2/3 h-screen bg-white">
+            <button @click="toggleNav">
+                <svg class="w-6 h-6 text-e-dark-grayish-blue" fill="currentColor" viewBox="0 0 20 20"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          fill-rule="evenodd" />
+                </svg>
+            </button>
+            <div class="py-12 space-y-4">
+                <h3 class="text-lg font-bold text-e-very-dark-blue">Collections</h3>
+                <h3 class="text-lg font-bold text-e-very-dark-blue">Men</h3>
+                <h3 class="text-lg font-bold text-e-very-dark-blue">Women</h3>
+                <h3 class="text-lg font-bold text-e-very-dark-blue">About</h3>
+                <h3 class="text-lg font-bold text-e-very-dark-blue">Contact</h3>
+            </div>
+        </div>
+    </nav>
+
+    <main :class="showNav === true ? 'blur' : 'blur-0'"
+          class="flex inset-0 h-screen bg-white">
+        <div class="flex m-auto max-w-7xl">
             <!-- cart item-->
-            <div :class="showCart === true ? 'block' : 'hidden'" class="absolute z-30 px-3 w-full">
-                <div class="mt-4 w-full max-w-md bg-white rounded-xl md:ml-auto">
-                    <h1 class="px-9 py-6 font-semibold border-b text-e-very-dark-blue">Cart</h1>
+            <div :class="showCart === true ? 'block' : 'hidden'"
+                 class="absolute right-0 top-20 z-20 px-3 w-full max-w-md md:top-32">
+                <div class="bg-white rounded-xl shadow-2xl md:ml-auto">
+                    <h1 class="px-9 py-6 font-semibold border-b-[2.5px] text-e-very-dark-blue">Cart</h1>
 
-                    <div class="flex justify-center items-center h-56 bg-white rounded-xl">
+                    <div class="flex justify-center items-center h-56">
                         <p class="font-semibold text-e-dark-grayish-blue">Your cart is empty.</p>
                     </div>
                 </div>
@@ -94,6 +103,29 @@
                             </svg>
                         </button>
                     </div>
+
+                    <!-- thumbnail images -->
+                    <div class="grid hidden relative grid-cols-4 gap-x-6 mt-6 md:inline-flex">
+                        <button class="rounded-2xl focus:ring-e-orange transition duration-300 ease-in-out delay-75 focus:opacity-60 hover:opacity-75 hover:-translate-y-1 hover:scale-110 focus:ring-[3px]">
+                            <img alt="Thumbnail 1" class="rounded-2xl"
+                                 src="../../../assets/images/ecommerce/image-product-1-thumbnail.jpg">
+                        </button>
+
+                        <button class="rounded-2xl focus:ring-e-orange transition duration-300 ease-in-out delay-75 focus:opacity-60 hover:opacity-75 hover:-translate-y-1 hover:scale-110 focus:ring-[3px]">
+                            <img alt="Thumbnail 2" class="rounded-2xl"
+                                 src="../../../assets/images/ecommerce/image-product-2-thumbnail.jpg">
+                        </button>
+
+                        <button class="rounded-2xl focus:ring-e-orange transition duration-300 ease-in-out delay-75 focus:opacity-60 hover:opacity-75 hover:-translate-y-1 hover:scale-110 focus:ring-[3px]">
+                            <img alt="Thumbnail 3" class="rounded-2xl"
+                                 src="../../../assets/images/ecommerce/image-product-3-thumbnail.jpg">
+                        </button>
+
+                        <button class="rounded-2xl focus:ring-e-orange transition duration-300 ease-in-out delay-75 focus:opacity-60 hover:opacity-75 hover:-translate-y-1 hover:scale-110 focus:ring-[3px]">
+                            <img alt="Thumbnail 4" class="rounded-2xl"
+                                 src="../../../assets/images/ecommerce/image-product-4-thumbnail.jpg">
+                        </button>
+                    </div>
                 </div>
 
                 <div class="p-6 bg-white md:pl-6 xl:pr-24 md:pr-12">
@@ -109,22 +141,24 @@
 
                     <div class="flex items-center py-4 mt-3">
                         <span class="text-3xl font-bold text-e-very-dark-blue">$125.00</span>
-                        <span class="p-1 ml-5 font-bold rounded-md bg-e-pale-orange text-e-orange">50%</span>
+                        <span class="px-2 py-1 ml-5 font-bold rounded-md bg-e-pale-orange text-e-orange">50%</span>
                     </div>
                     <span class="flex justify-end -mt-12 font-semibold line-through md:block md:-mt-0 text-e-grayish-blue">$250.00</span>
 
-                    <div class="flex flex-col md:flex-row">
+                    <div class="flex flex-col mt-4 md:flex-row">
                         <div class="flex justify-between items-center p-4 mt-3 rounded-md md:w-2/5 bg-e-light-grayish-blue">
-                            <button class="p-2" @click="quantity--">
+                            <button class="p-2 hover:opacity-75" @click="quantity--">
                                 <img alt="Minus" src="../../../assets/images/ecommerce/icon-minus.svg">
                             </button>
+
                             <p class="text-lg font-bold text-e-very-dark-blue">{{ quantity }}</p>
-                            <button class="p-2" @click="quantity++">
+
+                            <button class="p-2 hover:opacity-75" @click="quantity++">
                                 <img alt="Plus" src="../../../assets/images/ecommerce/icon-plus.svg">
                             </button>
                         </div>
 
-                        <button class="flex gap-x-4 justify-center items-center py-4 mt-4 w-full font-bold text-white rounded-md md:w-3/5 bg-e-orange">
+                        <button class="flex gap-x-4 justify-center items-center py-4 mt-4 w-full font-bold text-white rounded-md md:w-3/5 bg-e-orange hover:opacity-75 hover:shadow-2xl hover:shadow-orange-300">
                             <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2"
                                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
@@ -136,8 +170,8 @@
                     </div>
                 </div>
             </div>
-        </main>
-    </div>
+        </div>
+    </main>
 </template>
 
 <script>
